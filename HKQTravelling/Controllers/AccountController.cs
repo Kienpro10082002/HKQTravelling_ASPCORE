be0bf73f24f9a91b3a3e3921b3d524824b6d3902
Kienpro10082002 <Zuka10082002@gmail.com>
2023-11-01 11:32:42 +0700

    second commit

--- HKQTravelling/Controllers/AccountController.cs
+++ HKQTravelling/Controllers/AccountController.cs
@@ -1,9 +1,12 @@
-﻿using HKQTravelling.Models;
+﻿using HKQTravelling.Extension;
+using HKQTravelling.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Differencing;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using System.Security.Cryptography;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace HKQTravelling.Controllers
 {
@@ -34,11 +37,11 @@ namespace HKQTravelling.Controllers
         {
             string username = formCollection["UserName"];
             string password = formCollection["Password"];
-            string hashedPassword = GetMD5Hash(password);
+            string hashedPassword = Encrypt.GetMD5Hash(password);
             var dbUser = await data.users.FirstOrDefaultAsync(u => u.Username == username && u.Password == hashedPassword);
             if (dbUser != null)
             {
-                if (dbUser.RoleId == 1)
+                if (dbUser.RoleId == 1) // Nếu là admin
                 {
                     // Chuyển đổi đối tượng dbUser thành chuỗi JSON
                     var userJson = JsonConvert.SerializeObject(dbUser);
@@ -55,7 +58,11 @@ namespace HKQTravelling.Controllers
                     return RedirectToAction("Index", "Home");
                 }
             }
-            return View();
+            else
+            {
+                ViewData["LoginError"] = "Tên đăng nhập hoặc mật khẩu không đúng.";
+                return View();
+            }
         }
 
         [HttpPost]
@@ -70,14 +77,50 @@ namespace HKQTravelling.Controllers
             string gender = formCollection["Gender"].ToString();
             string birthdate = formCollection["BirthDate"].ToString();
             string niNumber = formCollection["NiNumber"].ToString();
-            if (checkUsername(username))
+
+            if (string.IsNullOrEmpty(username))
             {
-                ViewData["existed_user"] = "Tài khoản này đã tồn tại!";
+                ViewData["checking_user"] = "Tài khoản không được để trống!";
                 return View();
             }
-            else if (checkEmail(email))
+            else if (CheckAccountInformation.checkUsername(data, username))
             {
-                ViewData["existed_email"] = "Email này đã tồn tại";
+                ViewData["checking_user"] = "Tài khoản này đã tồn tại!";
+                return View();
+            }
+            else if (string.IsNullOrEmpty(email))
+            {
+                ViewData["checking_email"] = "Email không được để trống";
+                return View();
+            }
+            else if (CheckAccountInformation.checkEmail(data, email))
+            {
+                ViewData["checking_email"] = "Email này đã tồn tại";
+                return View();
+            }
+            else if (!CheckAccountInformation.IsEmailValid(email))
+            {
+                ViewData["checking_email"] = "Email không hợp lệ (có kí tự @ và sign)";
+                return View();
+            }
+            else if (string.IsNullOrEmpty(phone_number))
+            {
+                ViewData["checking_phonenumber"] = "Số điện thoại trống";
+                return View();
+            }
+            else if (!CheckAccountInformation.IsVietnamesePhoneNumber(phone_number))
+            {
+                ViewData["checking_phonenumber"] = "Số điện thoại không hợp lệ (SĐT VIỆT NAM)";
+                return View();
+            }
+            else if (CheckAccountInformation.checkSurname(surname) == false)
+            {
+                ViewData["checking_surname"] = "Họ và tên lót trống";
+                return View();
+            }
+            else if (CheckAccountInformation.checkName(name) == false)
+            {
+                ViewData["checking_name"] = "Tên trống";
                 return View();
             }
             else
@@ -85,15 +128,14 @@ namespace HKQTravelling.Controllers
                 var dbUser = new Models.Users
                 {
                     Username = username,
-                    Password = GetMD5Hash(password),
+                    Password = Encrypt.GetMD5Hash(password),
                     Status = 1,
                     CreationDate = Convert.ToDateTime(DateTime.Today),
-                    RoleId = 2 //Vai trò người dùng
+                    RoleId = 2 // Vai trò người dùng
                 };
 
                 var dbUserDetails = new Models.UserDetails
                 {
-
                     Email = email,
                     PhoneNumber = phone_number,
                     Surname = surname,
@@ -101,62 +143,13 @@ namespace HKQTravelling.Controllers
                     Gender = gender == "Nam" ? 1 : 2,
                     Birthdate = Convert.ToDateTime(birthdate),
                     NiNumber = niNumber,
-                    Age = caculateAge(Convert.ToDateTime(birthdate)),
+                    Age = Caculation.caculateAge(Convert.ToDateTime(birthdate)),
                     users = dbUser
                 };
                 data.users.Add(dbUser);
                 data.userDetails.Add(dbUserDetails);
                 await data.SaveChangesAsync();
                 return RedirectToAction("Login", "Account");
-            }
-        }
-        #endregion
-
-        #region functions
-        // Hàm mã hóa MD5
-        private string GetMD5Hash(string input)
-        {
-            if (string.IsNullOrEmpty(input))
-            {
-                // Nếu input rỗng hoặc null, trả về chuỗi rỗng.
-                return string.Empty;
-            }
-            using (var md5 = MD5.Create())
-            {
-                var dulieu = md5.ComputeHash(Encoding.UTF8.GetBytes(input));
-                var builder = new StringBuilder();
-
-                for (int i = 0; i < dulieu.Length; i++)
-                {
-                    builder.Append(dulieu[i].ToString("x2"));
-                }
-                return builder.ToString();
-            }
-        }
-        private bool checkUsername(string username)
-        {
-            return data.users.Count(u => u.Username == username) > 0;
-        }
-        private bool checkEmail(string email)
-        {
-            return data.userDetails.Count(u => u.Email == email) > 0;
-        }
-        private int caculateAge(DateTime birthDay)
-        {
-            if(birthDay == null)
-            {
-                return 0;
-            }
-            else
-            {
-                DateTime today = DateTime.Today;
-                int age = today.Year - birthDay.Year;
-
-                if (birthDay > today.AddYears(-age))
-                {
-                    age--;
-                }
-                return age;
             }
         }
         #endregion
---
+++ HKQTravelling/Extension/CheckAccountInformation.cs
@@ -0,0 +1,69 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Text.RegularExpressions;
+using HKQTravelling.Models;
+
+namespace HKQTravelling.Extension
+{
+    public static class CheckAccountInformation
+    {
+        public static bool checkUsername(ApplicationDBContext data, string username)
+        {
+            return data.users.Count(u => u.Username == username) > 0;
+        }
+        public static bool checkEmail(ApplicationDBContext data, string email)
+        {
+            return data.userDetails.Count(u => u.Email == email) > 0;
+        }
+        public static bool IsEmailValid(string email)
+        {
+            string pattern = @"^[\w\.-]+@[\w\.-]+\.\w+$";
+            return Regex.IsMatch(email, pattern);
+        }
+        public static bool checkSurname(string surname)
+        {
+            if (surname == null)
+            {
+                return false;
+            }
+            return true;
+        }
+        public static bool checkName(string name)
+        {
+            if (name == null)
+            {
+                return false;
+            }
+            return true;
+        }
+        public static bool existetPhoneNumber(ApplicationDBContext data, string phoneNumber)
+        {
+            return data.userDetails.Count(u => u.PhoneNumber == phoneNumber) > 0;
+        }
+        public static bool IsVietnamesePhoneNumber(string phoneNumber)
+        {
+            // Sử dụng biểu thức chính quy cho số điện thoại Việt Nam
+            // Biểu thức này kiểm tra số điện thoại di động Việt Nam bắt đầu bằng 0, sau đó là 9 số.
+            string pattern = @"^0\d{9}$";
+
+            // Kiểm tra số điện thoại sử dụng biểu thức chính quy
+            return Regex.IsMatch(phoneNumber, pattern);
+        }
+        public static bool existetNINumber(ApplicationDBContext data, string niNumber)
+        {
+            return data.userDetails.Count(u => u.NiNumber == niNumber) > 0;
+        }
+        public static bool numberOfNINumber(string niNumber)
+        {
+            int count = niNumber.Length;
+
+            if (count == 12)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
