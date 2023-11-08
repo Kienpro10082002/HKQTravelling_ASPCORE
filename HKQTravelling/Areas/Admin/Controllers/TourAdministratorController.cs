277268a5f5600c13593a2b572eb01e6b8b6bd092
Kienpro10082002 <Zuka10082002@gmail.com>
2023-11-08 12:34:08 +0700

    just init TourAdministrator file before merge

---
+++ HKQTravelling/Areas/Admin/Controllers/TourAdministratorController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HKQTravelling.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("TourAdministrator")]
+    public class TourAdministratorController : Controller
+    {
+        [HttpGet]
+        [Route("Index")]
+        public IActionResult Show()
+        {
+            return View();
+        }
+    }
+}
---
+++ HKQTravelling/Areas/Admin/Views/Shared/_NavigationPartialView.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HKQTravelling.Areas.Admin.Views.Shared
+{
+    public class _NavigationPartialViewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
